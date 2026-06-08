e4ea292241120ab458c3db8c2d4445312ba8af60
Cristian Lopez <crca9623@gmail.com>
2026-06-08 12:46:07 -0500

    Second test of compoenen MyCounterApp for call functions handleAdd, handleSubstract and handleReset

---
+++ src/counter/components/MyCounterApp2.test.tsx
@@ -0,0 +1,45 @@
+import { fireEvent, render, screen } from "@testing-library/react";
+import { describe, expect, test, vi } from "vitest";
+import { MyCounterApp } from "./MyCounterApp";
+
+const handleAddMock = vi.fn()
+const handleSubtractMock = vi.fn()
+const handleResetMock = vi.fn()
+
+vi.mock('../hooks/useCounter', () => ({
+  useCounter: () => ({
+    counter: 10,
+    handleAdd: handleAddMock,
+    handleSubtract: handleSubtractMock,
+    handleReset: handleResetMock
+  })
+})
+)
+
+describe('MyCounterApp', () => {
+  test('should render the component', () => {
+    render(<MyCounterApp />)
+
+    expect(screen.getByRole('heading', { level: 1 }).innerHTML).toContain(
+      'counter: 10'
+    )
+
+    expect(screen.getByRole('button', { name: '+1' })).toBeDefined()
+    expect(screen.getByRole('button', { name: '-1' })).toBeDefined()
+    expect(screen.getByRole('button', { name: 'Reset' })).toBeDefined()
+  })
+
+  test('should call function handleAdd if button +1 is cliked', () => {
+    render(<MyCounterApp />)
+
+    const button = screen.getByRole('button', { name: '+1' })
+
+    fireEvent.click(button)
+
+    expect(handleAddMock).toHaveBeenCalled()
+    expect(handleSubtractMock).not.toHaveBeenCalled()
+    expect(handleResetMock).not.toHaveBeenCalled()
+  })
+
+
+})
