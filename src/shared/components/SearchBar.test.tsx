c2a04ba626b7e655b4f5960ee1e0106a424a4fd7
Cristian Lopez <crca9623@gmail.com>
2026-06-14 07:52:58 -0500

    Created file SearchBar.test.tsx and validate funtionalitates of component

---
+++ src/shared/components/SearchBar.test.tsx
@@ -0,0 +1,67 @@
+import { act, fireEvent, render, screen, waitFor } from "@testing-library/react";
+import { describe, expect, test, vi } from "vitest";
+import { SearchBar } from "./SearchBar";
+
+
+describe('SearchBar', () => {
+  test('should render searchbar correctly ', () => {
+    const { container } = render(<SearchBar onQuery={() => { }} />)
+    expect(container).toMatchSnapshot()
+    expect(screen.getByRole('textbox')).toBeDefined()
+    expect(screen.getByRole('button')).toBeDefined()
+  })
+
+  test('should call onQuery whit the correct value after 700ms', async () => {
+    const onQuery = vi.fn()
+    render(<SearchBar onQuery={onQuery} />)
+
+    const input = screen.getByRole('textbox')
+    fireEvent.change(input, { target: { value: 'test' } })
+
+    await waitFor(() => {
+      expect(onQuery).toHaveBeenCalled()
+      expect(onQuery).toHaveBeenCalledWith('test')
+    })
+  })
+
+  test('should call only once whit the last value (debounce)', async () => {
+    const onQuery = vi.fn()
+    render(<SearchBar onQuery={onQuery} />)
+
+    const input = screen.getByRole('textbox')
+    fireEvent.change(input, { target: { value: 't' } })
+    fireEvent.change(input, { target: { value: 'te' } })
+
+    await waitFor(() => {
+      expect(onQuery).toHaveBeenCalledTimes(1)
+      expect(onQuery).toHaveBeenCalledWith('te')
+    })
+  })
+
+  test('should call onQuery when button cliked wit the input value', () => {
+    const onQuery = vi.fn()
+    render(<SearchBar onQuery={onQuery} />)
+
+    const input = screen.getByRole('textbox')
+    fireEvent.change(input, { target: { value: 'test' } })
+
+    const button = screen.getByRole('button')
+    fireEvent.click(button)
+
+    expect(onQuery).toHaveBeenCalledTimes(1)
+    expect(onQuery).toHaveBeenCalledWith('test')
+  })
+
+  test('should the input has the correct placeholder prop', () => {
+    const onQuery = vi.fn()
+    const value = 'Buscar'
+
+    render(<SearchBar onQuery={onQuery} placeholderText={value} />)
+
+    expect(screen.getByPlaceholderText(value)).toBeDefined()
+  })
+
+
+
+
+})
